bf8d9e533ba427452b6021f654af2257226f25cb
SaiRaviTejaG-gif <73626236+SaiRaviTejaG-gif@users.noreply.github.com>
2020-10-29 17:05:15 +0530

    Solution for problem 7
    Using basic Mathematics

---
+++ Problems/Problem_7/SaiRaviTejaG_Problem_7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem_Pal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the value of n: ");
+          n = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Sum: " + (n*(n+1))/2);
+
+          Console.ReadKey();
+
+        }
+    }
+}
